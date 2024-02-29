c756bc39f493b7a4ee0d72cceb87abb5db607ad8
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-02-29 19:58:02 +0100

    Add form validation and sendaing email script for netlify v2

--- dist/js/main.min.18dbc454.js
+++ dist/js/main.min.18dbc454.js
@@ -581,6 +581,7 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"1SICI":[function(require,module,exports) {
 var _navJs = require("./sections/nav.js");
 var _headerJs = require("./sections/header.js");
+var _contactmeJs = require("./sections/contactme.js");
 var _footerJs = require("./sections/footer.js");
 const init = function() {
     _navJs.mobileBtnHandler();
@@ -589,7 +590,7 @@ const init = function() {
 };
 init();
 
-},{"./sections/nav.js":"kwGx8","./sections/header.js":"6echj","./sections/footer.js":"eEEvq"}],"kwGx8":[function(require,module,exports) {
+},{"./sections/nav.js":"kwGx8","./sections/header.js":"6echj","./sections/footer.js":"eEEvq","./sections/contactme.js":"jg2qd"}],"kwGx8":[function(require,module,exports) {
 // --------- NAVIGATION
 // Elements
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
@@ -706,6 +707,88 @@ const updateYear = function() {
     spanYear.innerHTML = actualYear;
 };
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["iqNlW","1SICI"], "1SICI", "parcelRequirec63f")
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jg2qd":[function(require,module,exports) {
+// --------------------------------------------------------------
+// DOM Elements
+const form = document.querySelector('[data-js="contact-form"]');
+const name = document.querySelector('[data-js="contact-form__name"]');
+const email = document.querySelector('[data-js="contact-form__email"]');
+const message = document.querySelector('[data-js="contact-form__message"]');
+const submitBtn = document.querySelector('[data-js="contact-form__submit-btn"]');
+// -------------------------------------------------------------
+const emailURL = "/"; // "/" - url for netlify forms
+const ErrorMsg = {
+    fullname: "The Full name should contain at least 3 characters.",
+    email: "Please enter a correct email address.",
+    message: "The message should contain at least 10 characters."
+};
+// disable default html validation
+form.setAttribute("novalidate", true);
+const renderErrorMsg = function(element, msg) {
+    const parent = element.closest(".form__row");
+    const html = `                
+    <p class="error-msg">
+    ${msg}
+     </p>`;
+    parent.insertAdjacentHTML("beforeend", html);
+};
+const removeErrorMsg = function(element) {
+    const parent = element.closest(".form__row");
+    const errorMsg = parent.querySelector(".error-msg");
+    if (errorMsg) errorMsg.remove();
+};
+const checkRequiredFields = function() {
+    let isError = false;
+    if (!name.checkValidity()) {
+        isError = true;
+        renderErrorMsg(name, ErrorMsg.fullname);
+    } else removeErrorMsg(name);
+    if (!email.checkValidity()) {
+        isError = true;
+        renderErrorMsg(email, ErrorMsg.email);
+    } else removeErrorMsg(email);
+    if (!message.checkValidity()) {
+        isError = true;
+        renderErrorMsg(message, ErrorMsg.message);
+    } else removeErrorMsg(message);
+    return !isError;
+};
+const LoadingState = function(addState) {
+    form.classList[addState === true ? "add" : "remove"]("loading");
+    submitBtn.disabled = addState;
+};
+const makeRequest = async function(data) {
+    const res = await fetch(emailURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams(data).toString()
+    });
+    console.log(res);
+    console.log("submit form");
+    if (res.ok) return res.json();
+    throw new Error(`${res.status}: ${res.statusText}`);
+};
+const submitForm = async function() {
+    let isFormValid = checkRequiredFields();
+    if (isFormValid) {
+        const formData = new FormData(form);
+        LoadingState(true);
+        try {
+            const res = await makeRequest(formData);
+            console.log(res);
+        } catch (error) {
+            console.error(error);
+        }
+        LoadingState(false);
+    }
+};
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    submitForm();
+});
+
+},{}]},["iqNlW","1SICI"], "1SICI", "parcelRequirec63f")
 
 //# sourceMappingURL=main.min.18dbc454.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import * as nav from './sections/nav.js';
 import * as header from './sections/header.js';
+import * as contactme from './sections/contactme.js';
 import * as footer from './sections/footer.js';
 
 const init = function () {
