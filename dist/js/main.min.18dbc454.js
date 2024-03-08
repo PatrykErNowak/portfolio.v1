0ff1e6b097296aafa2c94ffddbc7a8b02656e9c5
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-03-08 12:47:23 +0100

    Improve nav mobile behavior: click on link will close navigation

--- dist/js/main.min.18dbc454.js
+++ dist/js/main.min.18dbc454.js
@@ -603,10 +603,20 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "mobileBtnHandler", ()=>mobileBtnHandler);
 const mobileBtn = document.querySelector('[data-js="nav-mobile-btn"]');
 const navlist = document.querySelector('[data-js="nav-list"]');
+const toggleMobileNav = function() {
+    mobileBtn.classList.toggle("is-active");
+    navlist.classList.toggle("active");
+};
+const clickLinkHandle = function(e) {
+    const isLink = e.target.hasAttribute("href");
+    if (!isLink) return;
+    toggleMobileNav();
+    navlist.removeEventListener("click", clickLinkHandle);
+};
 const mobileBtnHandler = function() {
     mobileBtn.addEventListener("click", (e)=>{
-        mobileBtn.classList.toggle("is-active");
-        navlist.classList.toggle("active");
+        toggleMobileNav();
+        navlist.addEventListener("click", clickLinkHandle);
         // A11Y
         const isExpanded = mobileBtn.getAttribute("aria-expanded") === "false" ? "true" : "false";
         mobileBtn.setAttribute("aria-expanded", isExpanded);
--- src/js/sections/nav.js
+++ src/js/sections/nav.js
@@ -3,10 +3,24 @@
 const mobileBtn = document.querySelector('[data-js="nav-mobile-btn"]');
 const navlist = document.querySelector('[data-js="nav-list"]');
 
+const toggleMobileNav = function () {
+  mobileBtn.classList.toggle('is-active');
+  navlist.classList.toggle('active');
+};
+
+const clickLinkHandle = function (e) {
+  const isLink = e.target.hasAttribute('href');
+
+  if (!isLink) return;
+
+  toggleMobileNav();
+  navlist.removeEventListener('click', clickLinkHandle);
+};
+
 export const mobileBtnHandler = function () {
   mobileBtn.addEventListener('click', (e) => {
-    mobileBtn.classList.toggle('is-active');
-    navlist.classList.toggle('active');
+    toggleMobileNav();
+    navlist.addEventListener('click', clickLinkHandle);
 
     // A11Y
     const isExpanded =
